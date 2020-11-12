04840da18500351008859f65383a04bd753e99af
Manuel <nulorincon@gmail.com>
2020-11-11 21:25:45 -0300

    deleted global variables not used

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/torre/';
-// $config['base_url'] = 'https://torretest.giraffema.com/manul00/';
+// $config['base_url'] = 'http://localhost/torre/';
+$config['base_url'] = 'https://torretest.giraffema.com/manul00/';
 
 /*
 |--------------------------------------------------------------------------
@@ -523,55 +523,6 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-
-################### VARIABLES GLOBALES DEL BMS ###################
-/**
- * Variables de correo
- */
-    $config['correo_nombre'] = 'BMS - Area de soporte';    
-    $config['correo_cuenta'] = 'soportebms@grupoprogestion.com';
-    $config['correo_clave'] = '8Gp4Zh1waQ';
-    $config['correo_protocolo'] = 'smtp';
-    $config['correo_crypto'] = 'tls';
-    $config['correo_host'] = 'smtp.office365.com';
-    $config['correo_port'] = '587';
-/**
- * Fin Variables de correo
- */
-
-/**
- * Integración con Captcha de Google
- */
-$config['google_key']    = '6LcfeskZAAAAABJb3g1TsN2kabML2wl_ISfGNRPY';
-$config['google_secret'] = '6LcfeskZAAAAAJcjAL24N7indGsroi-wohddMcWD';
-
-$config['google_key_inv']    = '6LfOg8kZAAAAAAwM8hixqQQn2gPOUWalZkUREewb';
-$config['google_secret_inv'] = '6LfOg8kZAAAAAG8L7CPL-MsPH2HKp7OUhy4LNezu';
-/**
-* Fin Integración con Captcha de Google
-*/
-
- /**
- * Variable de APIBMS
- */
-   
-   //$config['api_link'] = 'https://apiv2.bmspro.cl/bms/';
-   // $config['api_link'] = 'https://apibeta.bmspro.cl/bms/';
-   $config['api_link'] = 'http://localhost:4433/bms/';
-/**
- * Fin ariable de APIBMS
- */
-
-/**
- * Variables de Seleccion y Remuneraciones
- */
-    $config['tope_gratificacion'] = 126864;
-    $config['sueldo_minimo'] = 320500;
-    $config['porcentaje_calculo_grati'] = 0.25;
-/**
- * Fin Variables de Seleccion y Remunreaciones
- */
-################### FIN VARIABLES GLOBALES DEL BMS ###################
 
 
 
